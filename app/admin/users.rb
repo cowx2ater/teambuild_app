a7ab5e84c2a91fc69eb41b86cc7057a69aab13c8
wonno <jongwonno@likelion.org>
2018-04-28 05:21:39 +0900

    wonno

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -18,7 +18,7 @@ ActiveAdmin.register User do
       nil
     end
     column :position
-    actions
+    # actions
   end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
